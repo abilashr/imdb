7c18d34b4685763d15c8f4d87642449f1e543ea6
Abilash <abilashr@users.noreply.github.com>
2016-07-27 20:40:38 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 
 
 angular.module('movieApp',['ui.router'])
+.constant('myconfig', {
+  "url": 'http://www.omdbapi.com/'
+})
   .config(['$urlRouterProvider','$stateProvider', function($urlRouterProvider, $stateProvider){
     
     $urlRouterProvider.otherwise('/');
@@ -8,27 +11,25 @@ angular.module('movieApp',['ui.router'])
     $stateProvider.state('/' , {
       url: '/',
       templateUrl: 'search.html',
-      controller: ['$q','$scope', '$http', function($q, $scope, $http){
+      controller: ['$q','$scope', '$http', 'myconfig', function($q, $scope, $http, myconfig){
 
         var searchMovie = $scope.searchMovie;
 
         $scope.fetch = function(searchTxt){
           var q = $q.defer();
           console.log(searchTxt);
-          $http.post('http://www.omdbapi.com/?t='+ searchTxt)
+          $http.post(myconfig.url+'?t='+ searchTxt)
           .success(function(res){
             //console.log(res);
             q.resolve(res);
-            $scope.title = res.Title;
-            $scope.poster = res.Poster;
-            $scope.plot = res.Plot;
-
           }).error(function(err){
               q.reject({
                 error: err
               })
             })
-            return q.promise;
+            q.promise.then(function(res){
+              $scope.detail = res;
+            });
         }
         
       }]
